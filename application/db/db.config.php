98764d41cbb18cff39bbbb3ad3c3f25ee4fa7f88
Unknown <lbgamingde@outlook.com>
2017-09-30 10:35:17 +0200

    Edited DB connection

--- application/db/db.config.php
+++ application/db/db.config.php
@@ -1,13 +1,10 @@
 <?php
 // DB data [lb ; 29.09.2017]
-$sql_server = 'mysql:dbname=multiusers;host=127.0.0.1';
+$sql_server = 'localhost';
 $sql_user = 'root';
 $sql_password = '';
+$sql_dbname = '';
 
-    // Connect to db [lb ; 29.09.2017]
-try {
-    $db   = new PDO ($sql_server, $sql_user, $sql_password);
+// Connect to db [lb ; 29.09.2017]
+$db   = new PDO($sql_server, $sql_user, $sql_password);
 // Catch db erorr  [lb ; 29.09.2017]
-} catch (PDOExpection $e) {
-    die("Connection failed: " . $e->getMessage());
-}
--- application/module/user/View/user_login.php
+++ application/module/user/View/user_login.php
@@ -1,4 +1,7 @@
 <?php
+extract($_REQUEST);
+//echo '<pre>';
+//var_dump($_REQUEST['']);
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +13,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title> vSCP | Dashboard</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        
+
 
     </head>
     <body>
         <div>Hello World</div>
+
 
         
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
